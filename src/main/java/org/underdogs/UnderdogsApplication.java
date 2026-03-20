6bdadedc6265d8090bc46cf112e1895d439ebd80
ksarlary <ksarlary@gmail.com>
2026-03-20 22:17:22 +0100

    first commit

---
+++ src/main/java/org/underdogs/UnderdogsApplication.java
@@ -0,0 +1,13 @@
+package org.underdogs;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnderdogsApplication {
+    static void main(String[] args) {
+        SpringApplication.run(UnderdogsApplication.class, args);
+    }
+}
